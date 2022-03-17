5abf07326d9329792bb96091131cd66041e1d63a
Nithin <54320650+nithin-34@users.noreply.github.com>
2022-03-17 09:31:06 +0530

    updated path for html file

--- server.js
+++ server.js
@@ -70,8 +70,8 @@ app.get('/usersDat', function(req, resp){
 
 //Landing page view
 app.get('/', function(req, resp){
-    //console.log(__dirname + '\\views\\index.html');
-    resp.sendFile(__dirname + '\\views\\index.html');
+    console.log(__dirname + '\\views\\index.html');
+    resp.sendFile(__dirname + '//views//index.html');
   });
 
 //Node host and port defined
