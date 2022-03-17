6b3dad785a9565c8ed26d0dbda08c7f3fe0f081b
Nithin <54320650+nithin-34@users.noreply.github.com>
2022-03-17 21:43:03 +0530

    added laravel api data
    added node eventemitter and  sse for app

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
 
 let globalVal = 1;
 let localVal = 0;
@@ -20,14 +22,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/adduser', function(req, resp){
     globalVal++;
-    console.log("-------------");
-    console.log(users);
     let count =  (Object.keys(users.users)).length+1;
     users.users[count] = req.body.name;
     //users.total = ++(users.total);
     users['total']++;
-    console.log(users);
-    console.log("-------------");
+    emitter.emit('addeduser');
     resp.send(`Success: ${JSON.stringify(users)}`);
 
 })
@@ -46,27 +45,67 @@ app.get('/usersDat', function(req, resp){
     resp.setHeader('Cache-Control', 'no-cache');
     resp.setHeader('Connection', 'Keep-Alive');
     console.log('+++++++++++++++++');
-    intervalid  = setInterval(function() {
-        console.log(users);
-        let user1 = Object.values(users.users);
-        let names1 = user1.join(", ");
-        let total1 = users.total;
-        if(globalVal > localVal){
-            console.log("client connected");
-            //const date = new Date().toLocaleString()
-            //resp.write(`{"names":"${names}","total":${total}}\n\n`);
-            resp.write(`data: {"names":"${names1}","total":${total1},"globalVal":${globalVal}}\n\n`);
-            localVal = globalVal;
-        }
-      }, 100);
+    emitter.on('addeduser', () => {
+      //console.log("hey dude");
+      console.log("client connected");
+      let user1 = Object.values(users.users);
+      let names1 = user1.join(", ");
+      let total1 = users.total;
+
+      resp.write(`data: {"names":"${names1}","total":${total1},"globalVal":${globalVal}}\n\n`);
+    })
+//     intervalid  = setInterval(function() {
+//         console.log(users);
+//         let user1 = Object.values(users.users);
+//         let names1 = user1.join(", ");
+//         let total1 = users.total;
+//         if(globalVal > localVal){
+//             console.log("client connected");
+//             //const date = new Date().toLocaleString()
+//             //resp.write(`{"names":"${names}","total":${total}}\n\n`);
+//             resp.write(`data: {"names":"${names1}","total":${total1},"globalVal":${globalVal}}\n\n`);
+//             localVal = globalVal;
+//         }
+//       }, 100);
     
     resp.on('close', () => {
         console.log("client closed connection")
-        clearInterval(intervalid)
+       // clearInterval(intervalid)
         resp.end()
     })
 
   });
+
+// SSE events when laravel updated the status
+app.get('/orderStatus', (req, resp) => {
+  resp.setHeader('Content-Type', 'text/event-stream');
+  resp.setHeader('Access-Control-Allow-Origin', '*');
+  resp.setHeader('Cache-Control', 'no-cache');
+  resp.setHeader('Connection', 'Keep-Alive');
+  
+  console.log("client connected app");
+  emitter.on('order', (orderStatus) => {
+    //console.log("hey dude");
+//     let user1 = Object.values(users.users);
+//     let names1 = user1.join(", ");
+//     let total1 = users.total;
+
+    resp.write(`data: ${JSON.stringify(orderStatus)}\n\n`);
+  })
+
+  resp.on('close', () => {
+    console.log("client closed connection app")
+    resp.end()
+  })
+
+})
+
+// Api to get data from laravel api
+app.post('/statusUpdated', (req, resp) => {
+  resp.send("ok")
+  console.log(req.body);
+  emitter.emit('order', req.body);
+})
 
 //Landing page view
 app.get('/', function(req, resp){
